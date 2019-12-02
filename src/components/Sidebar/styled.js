bd8e72ada04a3f583e3cb7f607b3244f08c851ee
Guilherme Gomes <guiga.gomes10@gmail.com>
2019-12-02 00:20:14 -0300

    Sidebar

---
+++ src/components/Sidebar/styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const SidebarWrapper = styled.section`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    border-right: 1px solid #38444d;
+    background: #192734;
+    height: 100vh;
+    position: fixed;
+    padding: 2rem;
+    text-align: center;
+    width: 20rem;
+`
