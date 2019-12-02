0e7c08d068a7d88f76ed71a65f84ba9faf7bd965
Guilherme Gomes <guiga.gomes10@gmail.com>
2019-12-02 00:20:06 -0300

    Profile

---
+++ src/components/Profile/index.js
@@ -0,0 +1,39 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+import Avatar from "../Avatar"
+import * as S from "./styled"
+
+const Profile = () => {
+  const {
+    site: {
+      siteMetadata: { author, position, description },
+    },
+  } = useStaticQuery(graphql`
+    query MyQuery {
+      site {
+        siteMetadata {
+          author
+          position
+          description
+          title
+        }
+      }
+    }
+  `)
+
+  return (
+    <S.ProfileWrapper className="Profile-wrapper">
+      <S.ProfileLink>
+        <Avatar />
+        <S.ProfileAuthor>
+          {author}
+          <S.ProfilePosition>{position}</S.ProfilePosition>
+        </S.ProfileAuthor>
+      </S.ProfileLink>
+      <S.ProfileDescription>{description}</S.ProfileDescription>
+    </S.ProfileWrapper>
+  )
+}
+
+export default Profile
